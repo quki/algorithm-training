912e5d6e1215e848423efa2280f7cc7153b4bbe7
quki <quki09@gmail.com>
2016-09-09 17:19:47 +0900

    SCPC

--- src/quki/algorithm/scpc/SCPC2.java
+++ src/quki/algorithm/scpc/SCPC2.java
@@ -1,9 +1,12 @@
 package quki.algorithm.scpc;
 
-import java.util.Arrays;
 import java.util.Scanner;
+
 /**
- * 시간 초과
+ * DP 2차원 배열
+ * 1. mod 계산 주의 
+ * 2. 3중 for문 주의
+ * 
  * @author quki
  *
  */
@@ -22,41 +25,48 @@ public class SCPC2 {
 			int N = sc.nextInt();
 			int K = sc.nextInt();
 			int L = sc.nextInt();
+
 			int d[][] = new int[N + 1][K + 1];
-			boolean bomb[] = new boolean[N+1]; 
+			boolean bomb[] = new boolean[N + 1];
+			int sum[] = new int[N + 1];
+			int mod = 1000000009;
+
 			for (int i = 1; i <= L; i++) {
 				int b = sc.nextInt();
 				bomb[b] = true;
 			}
-			Arrays.fill(d[0], 1);
-			int mod = 1000000009;
-			for (int n = 1; n <= N; n++) {
-				
-				if(bomb[n])
+
+			// 초기화
+			if (!bomb[1]) {
+				d[1][1] = 1;
+				sum[1] = 1;
+			}
+
+			for (int n = 2; n <= N; n++) {
+
+				if (bomb[n])
 					continue;
-				
+
+				// 점화식
 				for (int k = 1; k <= K; k++) {
 
-					int sum = 0;
-
-					if (n - k >= 0) {
-						for (int j = 1; j <= K; j++) {
-							if (j != k) {
-								sum = (sum + d[n - k][j]) % mod;
-							}
-						}
+					if (n - k > 0) {
+						d[n][k] = (sum[n - k] + mod - d[n - k][k]) % mod;
 					}
-
-					d[n][k] = sum;
+					if (n == k) {
+						d[n][k] = 1;
+					}
 				}
+
+				// sum 계산
+				for (int k = 1; k <= K; k++) {
+					sum[n] = (sum[n] + d[n][k]) % mod;
+				}
+
 			}
 
 			System.out.println("Case #" + test_case);
-			int ans = 0;
-			for(int i = 1; i<=K ; i++){
-				ans+= d[N][i];
-			}
-			System.out.println(ans);
+			System.out.println(sum[N]);
 
 		}
 	}

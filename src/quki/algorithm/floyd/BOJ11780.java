15c7720360cb7f26e4c526c7a0ad75b1d806256c
quki <quki09@gmail.com>
2016-09-27 21:14:55 +0900

    Floyd-warshall

--- src/quki/algorithm/floyd/BOJ11780.java
+++ src/quki/algorithm/floyd/BOJ11780.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
  * Divide!
  * Conquer 라고 할 수 있나? OK!
  * 
+ * 플로이드 와샬문제는 동적계획법이다!
+ * https://ko.wikipedia.org/wiki/%ED%94%8C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%9B%8C%EC%85%9C_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98
+ * 
  * @author quki
  *
  */
